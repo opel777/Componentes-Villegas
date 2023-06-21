e05107ec04a1234c8b9902a809a1d03704c4c8e2
Gustavo Villegas <villegasgustavoopel@hotmail.com>
2023-06-21 10:30:59 -0300

    desafio-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { StudentsComponent } from './componentes/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule
